8126647ebefaa27f7aa3364998b31f6568c6ab13
Lukasz Smierzchala <lukasz.smierzchala@gmail.com>
2017-11-04 07:43:22 +0100

    add user controller

--- Source/RobotArm.WebApp/Controllers/UserController.cs
+++ Source/RobotArm.WebApp/Controllers/UserController.cs
@@ -3,15 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using RobotArm.WebApp.Models.Interfaces;
+using RobotArm.WebApp.ViewModels;
 
 namespace RobotArm.WebApp.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult Index()
+        private readonly IUserModel _userModel;
+
+        public UserController(IUserModel userModel)
         {
-            return View();
+            _userModel = userModel;
+        }
+       
+        public ActionResult Get()
+        {
+            UserViewModel[] userViewModels = _userModel.Get();
+            return View(userViewModels);
+        }
+
+        public ActionResult Details(int userId)
+        {
+            UserViewModel userViewModel = _userModel.Get(userId);
+            return View(userViewModel);
+        }
+
+        public ActionResult Edit(int userId)
+        {
+            UserViewModel userViewModel = _userModel.Get(userId);
+            return View(userViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserViewModel user)
+        {
+            _userModel.Update(user);
+            return View(user);
+        }
+
+        public ActionResult Create()
+        {
+            var userViewModel = new UserViewModel();
+            return View(userViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserViewModel user)
+        {
+            _userModel.Create(user);
+            return View(user);
+        }
+
+        [HttpPost]
+        public void Delete(int userId)
+        {
+            _userModel.Delete(userId);
         }
     }
 }
---
+++ Source/RobotArm.WebApp/Models/Interfaces/IUserModel.cs
@@ -0,0 +1,13 @@
+﻿using RobotArm.WebApp.ViewModels;
+
+namespace RobotArm.WebApp.Models.Interfaces
+{
+    public interface IUserModel
+    {
+        UserViewModel[] Get();
+        UserViewModel Get(int userId);
+        void Update(UserViewModel user);
+        void Create(UserViewModel user);
+        void Delete(int userId);
+    }
+}
